c2c8253fb975057dba4feca35cfade90a2c5d7b0
Kevia <jhampac@inbox.com>
2014-06-17 20:58:14 -0700

    some oop with php and javascript

--- www/page4.php
+++ www/page4.php
@@ -11,6 +11,55 @@
                     </div>
                     
                     <div class="panel-body">
+                    
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <?php
+                            
+                            class Car {
+                                
+                                public $make;
+                                public $model;
+                                
+                                function __construct( $make = '', $model = '') {
+                                    $this->make  = $make;
+                                    $this->model = $model;
+                                }
+                                
+                                public function start() {
+                                    echo "Car has started";
+                                }
+                                
+                                public function drive() {
+                                    echo "I am driving";
+                                }
+                                
+                                public function stop() {
+                                    echo "STOPPED";
+                                }
+                                
+                                public function __toString() {
+                                    return $this->make . " " . $this->model;
+                                }
+                            }
+                            
+                            $honda = new Car('honda', 'S2000');
+                            echo $honda;
+                            echo "<br>";
+                            $honda->start();
+                            echo "<br>";
+                            $honda->drive();
+                            echo "<br>";
+                            $honda->stop();
+                            echo "<br>";
+                            
+                             ?>
+                        </div>
+                     
+                         <div class="col-md-6 col-sm-6 col-xs-12">
+                            
+                            <p>The following is an attempt to do the same thing with javascript</p>
+                            
+                        </div>
                         
                     </div>
                     
