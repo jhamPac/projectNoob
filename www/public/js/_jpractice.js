641aa5245428e2cbf08056da9994ecfb4167f511
Kevia <jhampac@inbox.com>
2014-07-16 20:40:43 -0700

    slight change in variables

--- www/public/js/_jpractice.js
+++ www/public/js/_jpractice.js
@@ -1,23 +1,23 @@
 ;( function( $, window, undefined ) {
     'use strict';
     
-    var butt = {
+    var civic = {
     
         holder : $( "div.json textarea" ),
-        crap : $("div.json button.json_click" ),
+        action : $("div.json button.json_click" ),
         json : "",
     
         init : function () {
             
-            butt.crap.on( 'click', function() {
+            civic.action.on( 'click', function() {
                 $.ajax({
                    type : "GET",
                    url : "/public/json/info.json",
                    contentType: "application/json; charset=utf-8",
                    dataType : "text",
                    success : function( data ) {
-                       butt.json = $.parseJSON( data );
-                       butt.holder.text( butt.json.user[0].name + " " + butt.json.user[0].id + " " + butt.json.user[0].join_date );
+                       civic.json = $.parseJSON( data );
+                       civic.holder.text( civic.json.user[0].name + " " + civic.json.user[0].id + " " + civic.json.user[0].join_date );
                        
                    } 
                 });
@@ -27,6 +27,6 @@
     
     }
          
-    butt.init();
+    civic.init();
     
 } ( jQuery, this ) );
